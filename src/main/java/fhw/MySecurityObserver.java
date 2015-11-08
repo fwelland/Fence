8559ffd01735cf5ffebb001f185cda6d8c53184c
fwelland <fred.welland@gmail.com>
2015-11-07 21:24:29 -0500

    more tinkering around.

--- src/main/java/fhw/MySecurityObserver.java
+++ src/main/java/fhw/MySecurityObserver.java
@@ -1,33 +1,29 @@
 package fhw;
 
-//import javax.enterprise.event.Observes;
-//import org.picketlink.config.SecurityConfigurationBuilder;
-//import org.picketlink.event.SecurityConfigurationEvent;
+import javax.enterprise.event.Observes;
+import org.picketlink.config.SecurityConfigurationBuilder;
+import org.picketlink.event.SecurityConfigurationEvent;
 
 public class MySecurityObserver
 {
 
+    public MySecurityObserver() {}
     
-    void foo()
+    public void onInit(@Observes SecurityConfigurationEvent event)
     {
+        SecurityConfigurationBuilder builder = event.getBuilder();
+
+        builder
+                .http()
+                .forPath("/secure/*")
+                .authenticateWith()
+                .form()
+                .loginPage("/index.xhtml")
+                .errorPage("/error.xhtml")
+                .restoreOriginalRequest()
+                .forPath("/logout.xhtml")
+                .logout()
+                .redirectTo("/index.xhtml");
     }
-    
-//    public void onInit(@Observes SecurityConfigurationEvent event)
-//    {
-//        SecurityConfigurationBuilder builder = event.getBuilder();
-//
-//        builder
-//                .http()
-//                .allPaths()
-//                .authenticateWith()
-//                .form()
-//                .authenticationUri("/login.xhtml")
-//                .loginPage("/index.xhtml")
-//                .errorPage("/error.xhtml")
-//                .restoreOriginalRequest()
-//                .forPath("/logout.xhtml")
-//                .logout()
-//                .redirectTo("/index.xhtml");
-//    }
 
 }
