0e88c9a72e24f0e2b4f629f75266418f40797059
fwelland <fwelland@weedeater.wellands.com>
2015-11-15 22:06:11 -0500

    plumbing in some error stuff.

--- src/main/java/fhw/ELFunctions.java
+++ src/main/java/fhw/ELFunctions.java
@@ -4,31 +4,35 @@ import javax.faces.component.UIComponent;
 import javax.faces.component.UIInput;
 import javax.faces.component.UIViewRoot;
 import javax.faces.context.FacesContext;
+import org.omnifaces.util.Callback.WithArgument;
+import org.omnifaces.util.Components;
+import static org.omnifaces.util.Components.forEachComponent;
+
 
 public class ELFunctions
 {
     private ELFunctions() {}
     
-    public Boolean inputFailsValidation(UIInput input)
+    public static Boolean inputFailedValidation(final String inputId)
     {
-        Boolean b = Boolean.FALSE;
-        if(!input.isValid())
-        {
-            b = Boolean.TRUE; 
-        }
-        return(b);
-    }
-    
-    public Boolean inputFailedValidation(String inputId)
-    {
-        Boolean b = Boolean.FALSE;
-        UIViewRoot root = FacesContext.getCurrentInstance().getViewRoot();
-        UIComponent uic = root.findComponent(inputId);
-        if(UIInput.class.isInstance(uic))
-        {
-            b = inputFailsValidation((UIInput)uic);
-        }
-        return(b);        
+        final boolean[] b = new boolean[1];   
+        b[0] = true;
+        forEachComponent()
+            .ofTypes(UIInput.class)
+            .invoke(new WithArgument<UIComponent>() 
+            { 
+                @Override public void invoke(UIComponent component) 
+                {
+                    String id = component.getId();
+                    System.out.println("id is:  " + id);
+                    if(id.endsWith(inputId))
+                    {                        
+                        UIInput in = (UIInput)component;
+                        b[0] = in.isValid();
+                    }
+                }
+            });        
+        return b[0];
     }
     
 }
