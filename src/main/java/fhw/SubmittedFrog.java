88e98c0155eb07072cbb9a2917387668a6eeb508
fwelland <fwelland@weedeater.wellands.com>
2015-12-21 13:22:47 -0500

    misc updates

--- src/main/java/fhw/SubmittedFrog.java
+++ src/main/java/fhw/SubmittedFrog.java
@@ -1,17 +1,37 @@
 package fhw;
 
 import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.view.facelets.FaceletContext;
+import javax.inject.Inject;
 import javax.inject.Named;
 import lombok.Data;
-//import javax.validation.constraints.
+import javax.validation.constraints.Max;
 
 @Named
 @RequestScoped
-@Data 
+@Data
 public class SubmittedFrog
-{    
-    private String color; 
-    
-    private Integer val; 
-    
+{
+    private String color;
+
+    @Max(50)
+    private Integer val;
+
+
+    @Inject
+    private FacesContext facesContext;
+
+    public String getColor()
+    {
+        //facesContext.getExternalContext().getSessionMap()
+        System.out.println("getting color " + color);
+        return(color);
+    }
+
+    public void setColor(String c)
+    {
+        System.out.println("setting color with " + c );
+        color = c;
+    }
 }
